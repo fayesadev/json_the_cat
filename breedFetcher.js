c6360ced00ff7a13f83b1199cffa4a77bf102280
fayesadev <fayedumbrigue@gmail.com>
2023-01-19 23:18:32 +0000

    Refactored to move user-facing code into index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,25 @@
-const request = require('request');
-const input = process.argv.slice(2);
-
 //API url to return specific cat breeds:
 // https://api.thecatapi.com/v1/breeds/search
 //Search for a breed using '?q=<breedname>'
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${input[0]}`, (error, response, body) => {
-  //Edge Case: if request fails
-  if (error) {
-    console.log(error);
-    return;
-  }
-  //Edge Case: if Breed is not found
-  if (body === '[]') {
-    console.log("Error: Breed not found.");
-    return;
-  }
-  //HappyPath:
-  const data = JSON.parse(body); //converts string into object
-  /* data[0] gives us access to the object stored inside the data array */
-  console.log(data[0].description); //prints the description of breed
-});
+const request = require('request');
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    //Edge Case: if request fails
+    if (error) {
+      return callback(error);
+    }
+    //Edge Case: if Breed is not found
+    if (body === '[]') {
+      return callback("Breed not found.");
+    }
+    //HappyPath:
+    const data = JSON.parse(body); //converts string into object
+    /* data[0] gives us access to the object stored inside the data array */
+    return callback(null, data[0].description); //prints the description of breed
+  });
+};
+
+module.exports = { fetchBreedDescription };
