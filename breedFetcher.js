57eb7be479ebe9345e3e2381f731fd93ccf9409b
fayesadev <fayedumbrigue@gmail.com>
2023-01-19 22:13:15 +0000

    Created a node app that returns the description of a breed of cat from cat api when passed a breed

---
+++ breedFetcher.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const input = process.argv.slice(2);
+
+//API url to return specific cat breeds:
+// https://api.thecatapi.com/v1/breeds/search
+//Search for a breed using '?q=<breedname>'
+
+request(`https://api.thecatap93i.com/v1/breeds/search?q=${input[0]}`, (error, response, body) => {
+  //Edge Case: if request fails
+  if (error) {
+    console.log(error);
+    return;
+  }
+  //Edge Case: if Breed is not found
+  if (body === '[]') {
+    console.log("Error: Breed not found.");
+    return;
+  }
+  //HappyPath:
+  const data = JSON.parse(body); //converts string into object
+  /* data[0] gives us access to the object stored inside the data array */
+  console.log(data[0].description); //prints the description of breed
+});
